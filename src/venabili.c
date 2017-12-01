662a1cc7b876aa13be0bdca80c236536375c2fa7
Daniel Campoverde <alx@sillybytes.net>
2017-11-30 20:25:39 -0500

    Define keyboard and mouse report structures

--- src/venabili.c
+++ src/venabili.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/rcc.h>
@@ -26,7 +27,6 @@ int main(void)
 
 	while (1)
     {
-        // halt
     }
 }
 
@@ -49,6 +49,33 @@ int main(void)
 /* 	usbd_ep_write_packet(usbd_dev, 0x81, buf, 5); */
 /* } */
 
+typedef struct _keyboard_report_t
+{
+    uint8_t report_id;
+    uint8_t modifiers;
+    uint8_t _reserved;
+    uint8_t keycodes[6];
+}keyboard_report_t;
+
+typedef struct _mouse_report_t
+{
+    uint8_t report_id;
+    uint8_t buttons;
+    uint8_t x_axis;
+    uint8_t y_axis;
+    uint8_t wheel;
+}mouse_report_t;
+
+
+keyboard_report_t keyboard_report(uint8_t modifiers, uint8_t keycodes[6])
+{
+    keyboard_report_t kr = {0};
+    kr.report_id = 0x02;
+    kr._reserved = 0x00;
+    kr.modifiers = modifiers;
+    memcpy(kr.keycodes, keycodes, 6);
+    return kr;
+}
 
 // Send keyboard signal
 void sys_tick_handler(void)
@@ -57,13 +84,17 @@ void sys_tick_handler(void)
 	uint8_t buf[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 	buf[0] = 0x02;
 
+    uint8_t kcs[6] = {0x04, 0x00, 0x00, 0x00, 0x00, 0x00};
+    keyboard_report_t kr = keyboard_report(0x40, kcs );
+
     if (x == 0)
     {
         x = 1;
         // A down
         buf[1] = 0x40;
         buf[3] = 0x04;
-        usbd_ep_write_packet(usbd_dev, 0x81, buf, 9);
+        /* usbd_ep_write_packet(usbd_dev, 0x81, buf, 9); */
+        usbd_ep_write_packet(usbd_dev, 0x81, &kr, 9);
     }
     else
     {
@@ -74,6 +105,32 @@ void sys_tick_handler(void)
         usbd_ep_write_packet(usbd_dev, 0x81, buf, 9);
     }
 
+
+
+
+
+
+
+    /* static int x = 0; */
+	/* uint8_t buf[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; */
+	/* buf[0] = 0x02; */
+
+    /* if (x == 0) */
+    /* { */
+    /*     x = 1; */
+    /*     // A down */
+    /*     buf[1] = 0x40; */
+    /*     buf[3] = 0x04; */
+    /*     usbd_ep_write_packet(usbd_dev, 0x81, buf, 9); */
+    /* } */
+    /* else */
+    /* { */
+    /*     x = 0; */
+    /*     // A up */
+    /*     buf[1] = 0x40; */
+    /*     buf[3] = 0x00; */
+    /*     usbd_ep_write_packet(usbd_dev, 0x81, buf, 9); */
+    /* } */
 }
 
 // USB ISR handlers
