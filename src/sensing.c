069a7eb25b6313bf5e79260f13be07d6764869f9
Daniel Campoverde <alx@sillybytes.net>
2017-12-01 18:42:33 -0500

    Keys matrix sensing

--- src/sensing.c
+++ src/sensing.c
@@ -5,50 +5,6 @@
 
 bool keys_matrix[NROWS][NCOLS] = {0};
 
-void wipe_keys_matrix(void);
-
-int sense_keys(void)
-{
-    int n = 0;
-    wipe_keys_matrix();
-
-    // Row 1
-    gpio_set(GPIOB, GPIO0);
-    /* for (int i = 0; i < NCOLS; i++) */
-    /* { */
-        if (gpio_get(GPIOA, GPIO0))
-        {
-            keys_matrix[0][0] = true;
-        }
-    /* } */
-    gpio_clear(GPIOB, GPIO0);
-
-        /* if (keys_matrix[0][0]) */
-        /* { */
-        /*     gpio_set(GPIOB, GPIO2); */
-        /*     /1* report_key(usbd_dev, MOD_NONE, KEY_A); *1/ */
-        /* } */
-        /* else */
-        /* { */
-        /*     // Will only report the 6 out of 48 possible simultanious keys */
-        /*     // Becouse of USB 6KRO limitation */
-        /*     gpio_clear(GPIOB, GPIO2); */
-        /*     /1* report_key(usbd_dev, MOD_NONE, KEY_NONE); *1/ */
-        /* } */
-
-
-
-
-    /* gpio_set(GPIOB, GPIO1); */
-    /*     if (gpio_get(GPIOA, GPIO1)) */
-    /*     { */
-    /*         pressed_keys[n++] = B1; */
-    /*     } */
-    /* gpio_clear(GPIOB, GPIO1); */
-
-    return n;
-}
-
 void wipe_keys_matrix(void)
 {
     for (int i = 0; i < NROWS; i++)
@@ -58,6 +14,28 @@ void wipe_keys_matrix(void)
             keys_matrix[i][j] = false;
         }
     }
+}
+
+int sense_keys(void)
+{
+    int n = 0;
+    wipe_keys_matrix();
+
+    // Row 1
+    for (int r = 0; r < NROWS; r++)
+    {
+        gpio_set(GPIOB, 1 << r);
+        for (int c = 0; c < NCOLS; c++)
+        {
+            if (gpio_get(GPIOA, 1 << c))
+            {
+                keys_matrix[r][c] = true;
+                n++;
+            }
+        }
+        gpio_clear(GPIOB, 1 << r);
+    }
+    return n;
 }
 
 /* void mapping(void) */
