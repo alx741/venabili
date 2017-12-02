87842889b2e95e94f4d11d5354dfa1148097686f
Daniel Campoverde <alx@sillybytes.net>
2017-12-01 19:42:02 -0500

    Move port configuration to keyboard_sensing_int()

--- src/sensing.c
+++ src/sensing.c
@@ -1,9 +1,26 @@
 #include <string.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 #include "sensing.h"
 #include "keys.h"
 
 bool keys_matrix[NROWS][NCOLS] = {0};
+
+void keyboard_sensing_init(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+    // PORTA (0-11) as matrix inputs
+	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN,
+            GPIO0 | GPIO1 | GPIO2 | GPIO3 | GPIO4 | GPIO5 | GPIO6 | GPIO7
+            | GPIO8 | GPIO9 | GPIO10 | GPIO11);
+
+    // PORTB (0-3) as matrix outputs
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL,
+            GPIO0 | GPIO1 | GPIO2 | GPIO3);
+	gpio_clear(GPIOB, GPIO0 | GPIO1 | GPIO2 | GPIO3);
+}
 
 void wipe_keys_matrix(void)
 {
