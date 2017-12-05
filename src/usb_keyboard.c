c4607a4ff72b9b31110e3f8634cb7ea4d493f79a
Daniel Campoverde <alx@sillybytes.net>
2017-12-05 09:46:52 -0500

    Make USB keyboard report structure private

--- src/usb_keyboard.c
+++ src/usb_keyboard.c
@@ -3,6 +3,14 @@
 #include "usb_keyboard.h"
 #include "usb_keys.h"
 
+typedef struct _keyboard_report_t
+{
+    uint8_t report_id;
+    uint8_t modifier;
+    uint8_t _reserved;
+    uint8_t keycodes[6];
+} keyboard_report_t;
+
 void report_key(usbd_device* usbd_dev, uint8_t modifier, uint8_t keycode)
 {
     keyboard_report_t kr = {0};
