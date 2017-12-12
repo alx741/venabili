659ad6569b72e24fede2d945a281f746f59f10a8
Daniel Campoverde <alx@sillybytes.net>
2017-12-11 21:42:01 -0500

    Declare report_mouse_click

--- src/usb_mouse.h
+++ src/usb_mouse.h
@@ -3,9 +3,8 @@
 
 typedef enum _mouse_axis {UP, DOWN, LEFT, RIGHT, WHEELUP, WHEELDOWN} MouseMovement;
 
-/* Report mouse movement
- */
 void report_mouse_movement(MouseMovement m, uint8_t speed);
+void report_mouse_click(uint8_t button);
 
 #endif // USB_MOUSE_H
 
