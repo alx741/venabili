39a403f875381d46bf849203187625d4d79187d4
Daniel Campoverde <alx@sillybytes.net>
2019-02-17 12:22:30 -0500

    Check for important keys

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -5,13 +5,27 @@ source "$(dirname "$0")/fancy.sh"
 layers_file=../src/venabili.c
 warnings_counter=0
 
-if [[ $(grep -i c_flash_mode "$layers_file") == "" ]]
-then
-    displayWarning "There is no flash mode key (c_flash_mode)"
-    warnings_counter=$((warnings_counter+1))
-fi
+function check_key
+{
+    if [[ $(grep -i "$1" "$layers_file") == "" ]]
+    then
+        displayWarning "There is no $2 key ($1)"
+        warnings_counter=$((warnings_counter+1))
+    else
+        displayInfo "$2 key available"
+    fi
+
+}
+
+# Important keys
+check_key "c_flash_mode" "Flash mode"
+check_key "k_enter"      "Enter"
+check_key "k_space"      "Space"
+check_key "k_backspace"  "Backspace"
+check_key "k_delete"     "Delete"
 
 if [[ $warnings_counter -eq 0 ]]
 then
+    echo
     displaySuccess "All checks passed"
 fi
